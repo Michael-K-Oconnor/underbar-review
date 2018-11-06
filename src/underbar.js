07e98072b0bf6d0e26a40d5e027538f07363f2a3
michael-k-oconnor <m.k.oconnor53@gmail.com>
2018-11-06 16:34:01 -0600

    Add functionality to functions up until reduce.

--- src/underbar.js
+++ src/underbar.js
@@ -141,6 +141,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+    _.each(collection, function(element){
+      result.push(iterator(element));
+    });
+    return result;
   };
 
   /*
@@ -182,6 +187,30 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if (iterator) {
+      var collection = _.map(collection,iterator);
+    }
+
+    if (!accumulator) {
+      accumulator = collection[0];
+      for (var i = 1; i < collection.length; i++) {
+        if (iterator) {
+          accumulator += iterator(accumulator,collection[i]);
+        } else {
+          accumulator += collection[i];
+        }
+      }
+
+    } else {
+        for (var i = 0; i < collection.length; i++) {
+          if (iterator) {
+            accumulator += iterator(accumulator,collection[i]);
+          } else {
+            accumulator += collection[i];
+          }
+        }
+    }
+    return accumulator
   };
 
   // Determine if the array or object contains a given value (using `===`).
